2c7f4805c6a80cb4f060a278cb96a66e7801bbab
nastinkaKrd <nastinka352@gmail.com>
2023-10-08 10:31:46 +0300

    task1.4(For this I use threads. Also changed variables names, and changed checking data length)

--- client.py
+++ client.py
@@ -1,15 +1,14 @@
 import socket
 
-clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 host = "127.0.0.1"
-port = 1025
-clientSocket.connect((host, port))
+port = 1027
+client_socket.connect((host, port))
 while True:
     message = input("Введіть текст для сервера (або 'exit' для виходу): ")
     if message == 'exit':
         break
-    message += '\n'
-    clientSocket.send(message.encode('utf-8'))
+    client_socket.send(message.encode('utf-8'))
 
-clientSocket.close()
+client_socket.close()
--- server.py
+++ server.py
@@ -1,25 +1,33 @@
 import socket
 import datetime
 import time
-
-# in commit 2 task 1.3 also
-serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+import threading
 
 host = "127.0.0.1"
-port = 1025
-serverSocket.bind((host, port))
-serverSocket.listen(1)
-print(f"Сервер прослуховує на {host}:{port}")
-clientSocket, clientAddress = serverSocket.accept()
-print(f"З'єднано з {clientAddress}")
-while True:
-    dataFromClient = clientSocket.recv(1024)
-    if not dataFromClient:
-        break
-    time.sleep(5)
-    if dataFromClient.endswith(b'\n'):
-        currentTime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        print(f"Отримано від клієнта повідомлення {dataFromClient.decode('utf-8')} ({currentTime})")
+port = 1027
 
-clientSocket.close()
-serverSocket.close()
+
+def handle_client(client_socket, client_address):
+    print(f"З'єднано з {client_address}")
+    try:
+        while True:
+            data_from_client = client_socket.recv(1024)
+            time.sleep(5)
+            if len(data_from_client) > 0:
+                current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                print(f"Отримано від клієнта повідомлення {data_from_client.decode('utf-8')} ({current_time})")
+                clientSocket.sendall(data_from_client)
+    except ConnectionResetError:
+        pass
+    finally:
+        print(f"Закрито з'єднання з {clientAddress}")
+        clientSocket.close()
+
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as serverSocket:
+    serverSocket.bind((host, port))
+    serverSocket.listen(2)
+    while True:
+        clientSocket, clientAddress = serverSocket.accept()
+        t = threading.Thread(target=handle_client, args=(clientSocket, clientAddress))
+        t.start()
