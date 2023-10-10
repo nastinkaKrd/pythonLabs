abed6ab013a3d773a1e09e695ff3fef8ee5b1cbc
nastinkaKrd <nastinka352@gmail.com>
2023-10-10 10:50:09 +0300

    task 2 (I changed form_handler.py to add and delete cookies with counter of filled forms and max-age of life cookies)

--- cgi-bin/form_handler.py
+++ cgi-bin/form_handler.py
@@ -1,13 +1,36 @@
 import cgi
 import cgitb
+import http.cookies
+import os
+import datetime
 
 cgitb.enable()
 
 form = cgi.FieldStorage()
+
+cookie = http.cookies.SimpleCookie(os.environ.get("HTTP_COOKIE"))
+counter = int(cookie.get("counter", 0))
+
 try:
-    email = form.getvalue("email", "error")
-    password = form.getvalue("password", "error")
-    season = form.getvalue("season", "error")
+    if "email" in form:
+        counter += 1
+        cookie["counter"] = str(counter)
+        cookie["counter"]["max-age"] = 3600
+    if "password" in form:
+        counter += 1
+        cookie["counter"] = str(counter)
+        cookie["counter"]["max-age"] = 3600
+    if "season" in form:
+        counter += 1
+        cookie["counter"] = str(counter)
+        cookie["counter"]["max-age"] = 3600
+    if "fruit" in form:
+        counter += 1
+        cookie["counter"] = str(counter)
+        cookie["counter"]["max-age"] = 3600
+    email = form.getvalue("email", "error (data is not found)")
+    password = form.getvalue("password", "error (data is not found)")
+    season = form.getvalue("season", "error (data is not found)")
     fruit = form.getlist("fruit")
 except (NameError, KeyError) as e:
     message = e
@@ -29,7 +52,17 @@ html = f"""
         <li><strong>Season:</strong> {season}</li>
         <li><strong>Fruits:</strong> {fruit}</li>
     </ul>
+    <p>Filled forms: {counter}</p>
+    <form method="post" action="">
+        <input type="submit" name="clear_cookies" value="Delete all cookies">
+    </form>
 </body>
 </html>
 """
-print(html.format(email=email, password=password, season=season, fruit=', '.join(fruit)))
+if "clear_cookies" in form:
+    cookie["counter"] = ""
+    cookie["counter"]["expires"] = str(datetime.datetime.now())
+
+print(cookie.output())  
+print()
+print(html.format(email=email, password=password, season=season, fruit=', '.join(fruit), counter=counter))
